096f356df0b7474ed3e72104459bc7567d60ba7f
KSI <78685953+sergharkov@users.noreply.github.com>
2023-04-19 14:56:50 +0300

    Update index_total.php

--- php+SQL/index_total.php
+++ php+SQL/index_total.php
@@ -1,22 +1,46 @@
 <?php
 $comparevalue = getenv("TIME_MONITOR");#0.9;
+$servername = getenv("MYSQL_HOST"); // "dev_mysql";
+$username   = getenv("MYSQL_USER"); // "stage_";
+$dbname     = getenv("MYSQL_BASE"); // "stage_";
+$password   = getenv("MYSQL_PASS"); // "xxxxxxxxxxxx";
 $logfile = '/var/logs/status.log';
-$telegramapiToken= 'xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$telegramapiToken= 'XXXXXXXXXXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
 echo "++++++++++++++++++++++++++Сheck time select tblnotes than not more then $comparevalue ++++++++++++++++++++++++++++++++++<p>";
 /////////////////////////////////////////////////////////////////////////////
+function GETVALUEARRAY(){
+global $logfile;
+$arr = array();
+echo "</br>$logfile";
+$userfile= file_get_contents($logfile); 
+   // Place each line of $userfile into array
+   $users = explode("\n",$userfile);
+   foreach ($users as $user) {
+      list($date, $time, $s4, $s5, $s6, $s7, $s8, $s9, $s10, $s11, $s12, $s13, $s14, $DB, $s16, $lag) = explode(" ",$user);
+      $fulltime = $date."-".$time;
+      //$arr[$lag] = $fulltime;
+      //array_push($arr, array($lag,$fulltime));
+      $arr[] = array ('y' =>$lag, 'label'=>$fulltime);
+   }
+//print_r($arr[100]);
+//print_r(array_key($arr));
+//print_r(array_values($arr));
+return $arr;
+}
+/////////////////////////////////////////////////////////////////////////////////
 function LOGSTOFILE ($timevalue, $i, $dbname,$teltextstring){
 global $comparevalue;	
 global $logfile;
-
 $date_start = new DateTimeImmutable();
 // Open the file to get existing content
 $current = file_get_contents($logfile);
 	if ($timevalue < $comparevalue) {
-                echo "</br>SELECT TIME less then $comparevalue";
+                echo "</br>LOGSTOFILE: SELECT TIME less then $comparevalue";
 		// Append a new person to the file
                 $current .= date_format($date_start, 'Y-m-d H:i:s')."  OKKK    $teltextstring\n";
 	} else {
-                echo "</br>SELECT TIME more $timevalue then $comparevalue";
+                echo "</br>LOGSTOFILE: SELECT TIME more $timevalue then $comparevalue";
     // Append a new person to the file
                 $current .= date_format($date_start, 'Y-m-d H:i:s')."  ALARM    $teltextstring\n";
 	}
@@ -28,9 +52,9 @@ function TELEGRAMSEND ($timevalue, $i, $dbname,$teltextstring){
 global $comparevalue;
 global $telegramapiToken;
 if ($timevalue < $comparevalue) {
-        	echo "</br>SELECT TIME less then $comparevalue";
+        	echo "</br>TELEGRAM: SELECT TIME less then $comparevalue";
 	} elseif ($timevalue > $comparevalue) {
-        	echo "</br>SELECT TIME more then $comparevalue";
+        	echo "</br>TELEGRAM: SELECT TIME more then $comparevalue";
 //Send status by telegram bot curl POST
   $data = [
       'chat_id' => '-1001520271519',
@@ -59,8 +83,8 @@ if(! $retval ) {
 }
 echo "Database <b>$dbname</b> selected successfully\n";
 /////////////////////////////// SQL //////////////////////////
-$sql = "SELECT * from tblnotes
-       LIMIT 0, 100"; //tblleads ";// LIMIT 0, 100";                                                     
+$sql ="SELECT * from tblnotes
+//       LIMIT 0, 100"; //tblleads ";// LIMIT 0, 100";                                                     
 /////////////////////////////// SQL //////////////////////////
 echo "<p> RAW SLQ >>> $sql <p>";
 
@@ -84,12 +108,54 @@ echo "</br>$teltextstring";
 TELEGRAMSEND ($time_elapsed_secs, $i, $dbname,$teltextstring);
 LOGSTOFILE ($time_elapsed_secs, $i, $dbname,$teltextstring);
 }
-echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%<p>";
-
-$servername = getenv("MYSQL_HOST"); // "dev_mysql";
-$username   = getenv("MYSQL_USER"); // "stage_";
-$dbname     = getenv("MYSQL_BASE"); // "stage_";
-$password   = getenv("MYSQL_PASS"); // "xxxxxxxxxxxx";
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<p>";
-SQLQUERY($servername, $dbname,$username, $password);
 ?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<?php SQLQUERY($servername, $dbname,$username, $password);  ?>
+
+</br>
+<form method="post">
+    <input type="submit" name="show" value="show"/>
+    <input type="submit" name="NoShow" value="noshow"/>
+</form>
+
+<?php
+    if(isset($_POST['show'])){
+            echo "Select buttom show";
+	    $arrres=GETVALUEARRAY();
+	    print_r($arrres);
+    } else {
+            echo "Select buttom no show";
+    }
+?>
+<!--From https://canvasjs.com/php-charts/chart-zoom-pan/ -->
+<?php
+    $arrret= GETVALUEARRAY();
+?>
+<script>
+window.onload = function () {
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	animationEnabled: true,
+	zoomEnabled: true,
+	title: {
+		text: "Try Zooming and Panning"
+	},
+	data: [{
+		type: "area",     
+		dataPoints: <?php echo json_encode($arrret, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html> 
