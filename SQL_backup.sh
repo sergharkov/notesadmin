9348a3ae08a46ddedaa33277a0fce34917b0f349
KSI <78685953+sergharkov@users.noreply.github.com>
2023-06-12 17:44:04 +0300

    Update SQL_backup.sh

--- SQL_backup.sh
+++ SQL_backup.sh
@@ -6,6 +6,8 @@ dbname="xxxx"
 dbpass="xxxx"
 envdomain=$(hostname)
 OUTSIDEneedBackUP=""
+alertdiskspace=""
+result_parth_over=""
 remoteBackUpIP="xxx.xxx.xxx.xxx"
 remoteBackUpPath="/home/services/mysql_dump/rrrrrrrr/"
 
@@ -16,7 +18,6 @@ mkdir -p $current_folder
 backupfile="$current_folder/$envdomain-$(date +%d-%m-%Y--%H-%M).dump"
 
 HoursBackUP=$(date +%H)
-
 PGPASSWORD=$dbpass pg_dump -Fc --host=$host --port=$port --username=$dbuser $dbname --file=$backupfile
 
 if (( $HoursBackUP % 4  == 0 ))           # no need for backups
@@ -28,6 +29,14 @@ then
 else
 	echo "simple permanenly bachUP to local PATH"
 fi
-curl -s -X POST https://api.telegram.org/bot6040401992:AAEQM0olDtE3eDh5a2asoAtuH7hHYsrhCYQ/sendMessage -d chat_id=-808288196 -d text="PostgreSQL DB:$dbname backup from env:$envdomain done $OUTSIDEneedBackUP"
 
+result_parth_over=$(df -h --output=source,pcent | awk '0+$2 >= 75 {print}')
+if [ -z "$result_parth_over" ]
+then
+      echo "$result_parth_over is NOT full"
+else
+      alertdiskspace=$(echo "!!!!! $result_parth_over is full")
+      echo "$result_parth_over is full"
+fi
+curl -s -X POST https://api.telegram.org/bot6040401992:AAEQM0olDtE3eDh5a2asoAtuH7hHYsrhCYQ/sendMessage -d chat_id=-808288196 -d text="PostgreSQL DB:$dbname backup from env:$envdomain done $OUTSIDEneedBackUP"
 echo "$(date +%d-%m-%Y--%H-%M)  PostgreSQL DB:$dbname backup from env:$envdomain done" >> /var/log/postgreSQLbackup.log
