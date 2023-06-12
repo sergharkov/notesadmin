81f2de9f7e0f5482b303a10a679254b28bb8e0f7
KSI <78685953+sergharkov@users.noreply.github.com>
2023-06-12 16:05:47 +0300

    Create SQL_backup.sh

---
+++ SQL_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+host="0.0.0.0"
+port="5433"
+dbuser="xxxx"
+dbname="xxxx"
+dbpass="xxxx"
+envdomain=$(hostname)
+OUTSIDEneedBackUP=""
+remoteBackUpIP="xxx.xxx.xxx.xxx"
+remoteBackUpPath="/home/services/mysql_dump/rrrrrrrr/"
+
+
+current_folder="/autostart/postgreSQL/backup/$(date +%Y-%m-%d)/$(date +%H-%M-%S)"
+#current_folder="/autostart/postgreSQL/backup/$(date +%Y)/$(date +%m)/$(date +%d-%m-%Y)"
+mkdir -p $current_folder
+backupfile="$current_folder/$envdomain-$(date +%d-%m-%Y--%H-%M).dump"
+
+HoursBackUP=$(date +%H)
+
+PGPASSWORD=$dbpass pg_dump -Fc --host=$host --port=$port --username=$dbuser $dbname --file=$backupfile
+
+if (( $HoursBackUP % 4  == 0 ))           # no need for backups
+then
+	echo $HoursBackUP
+	OUTSIDEneedBackUP="and backup replicas to outside server"
+	echo "$(date +%d-%m-%Y--%H-%M)  PostgreSQL DB:$dbname need backup from env:$envdomain TO OUTSIDE server $remoteBackUpIP" >> /var/log/postgreSQLbackup.log
+	scp $backupfile $remoteBackUpIP:$remoteBackUpPath
+else
+	echo "simple permanenly bachUP to local PATH"
+fi
+curl -s -X POST https://api.telegram.org/bot6040401992:AAEQM0olDtE3eDh5a2asoAtuH7hHYsrhCYQ/sendMessage -d chat_id=-808288196 -d text="PostgreSQL DB:$dbname backup from env:$envdomain done $OUTSIDEneedBackUP"
+
+echo "$(date +%d-%m-%Y--%H-%M)  PostgreSQL DB:$dbname backup from env:$envdomain done" >> /var/log/postgreSQLbackup.log
